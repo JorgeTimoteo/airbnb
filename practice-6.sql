0a1fbeb71f1bdc071b27967d3ea757215add2ef4
Jorge Timoteo <jorge.timoteoprado@kellogg.northwestern.edu>
2023-04-14 02:16:08 +0000

    airbnb_done

--- practice-6.sql
+++ practice-6.sql
@@ -15,4 +15,7 @@
 -- | Brighton Park          | 678      |
 -- | Burnside               | 10       |
 
+SELECT listings.neighborhood, count(listings.id)
+FROM listings inner join reviews ON listings.id = reviews.listing_id
+GROUP by listings.neighborhood
 
--- practice-7.sql
+++ practice-7.sql
@@ -16,4 +16,6 @@
 -- | Entire townhouse                    | 3588     | 2021-10-17                 |
 -- | Entire villa                        | 75       | 2021-10-12                 |
 
-
+SELECT listings.property_type, count(reviews.listing_id), max(reviews.date_reviewed)
+FROM listings inner join reviews ON listings.id = reviews.listing_id
+GROUP BY listings.property_type
